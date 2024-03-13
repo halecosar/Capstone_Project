c2e0320c74a731cdf6e0c03ecea794edab4f9020
halecosar <halealtunakar@hotmail.com>
2024-03-13 21:07:51 +0300

    delete api

--- API/src/main/java/dev/patika/veterinary_project/core/config/modelMapper/IModelMapperService.java
+++
@@ -1,8 +0,0 @@
-package dev.patika.veterinary_project.core.config.modelMapper;
-
-import org.modelmapper.ModelMapper;
-
-public interface IModelMapperService {
-    ModelMapper forRequest();
-    ModelMapper forResponse();
-}
--- API/src/main/java/dev/patika/veterinary_project/dao/IVaccineRepo.java
+++
@@ -1,9 +0,0 @@
-package dev.patika.veterinary_project.dao;
-
-import dev.patika.veterinary_project.entities.Vaccine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IVaccineRepo extends JpaRepository<Vaccine,Long> {
-}
